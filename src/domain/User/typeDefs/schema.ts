03161fa17f7077cba48479560d86f0ff7edbc7b3
Pedro Mihael <pedromvpaes@gmail.com>
2021-01-19 17:43:03 -0400

    feat: list all users from stub

--- src/domain/User/typeDefs/schema.ts
+++ src/domain/User/typeDefs/schema.ts
@@ -2,15 +2,15 @@ import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
   type Query {
-    # listar: [Users!]!
-    listar(name: String!): User
+    listar: [User!]!
+    # listar(name: String!): User
   }
 
   type User {
     _id: ID!
     index: Int!
     picture: String
-    age: String!
+    age: Int!
     eyeColor: String
     name: String!
     company: String
--- src/useCases/User/resolvers/ListUsersResolver.ts
+++ src/useCases/User/resolvers/ListUsersResolver.ts
@@ -1,8 +1,10 @@
+const users = [
+  { _id: 1, index: 0, age: 21, email: 'email1', phone: '123123', name: 'Pedro' },
+  { _id: 2, index: 1, age: 27, email: 'email2', phone: '456456', name: 'Camila' },
+];
+
 export default {
   Query: {
-    listar(name: string) {
-      if (name) return name;
-      else return 'todos os usuarios';
-    },
+    listar: () => users,
   },
 };
