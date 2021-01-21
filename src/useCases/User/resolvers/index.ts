91d84221ae586edaac8c38af4cff33860ec57329
Pedro Mihael <pedromvpaes@gmail.com>
2021-01-21 13:49:17 -0400

    chore: add resolver dependencies resolver

--- src/useCases/User/resolvers/index.ts
+++ src/useCases/User/resolvers/index.ts
@@ -1,3 +1,11 @@
-import UserResolvers from './ListUsersResolver';
+import { container } from 'tsyringe';
+import ListUsersResolvers from './ListUsersResolver';
 
-export default UserResolvers;
+export default class Resolvers {
+  async getResolvers() {
+    const userResolver = container.resolve(ListUsersResolvers);
+    const dependenciesResolvedUserResolver = await userResolver.execute();
+
+    return { users_list: dependenciesResolvedUserResolver };
+  }
+}
