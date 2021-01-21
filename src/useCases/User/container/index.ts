323000a7aed65510e756b58d7ebbc7e8f5dcd8a6
Pedro Mihael <pedromvpaes@gmail.com>
2021-01-21 13:48:24 -0400

    chore: add container dependencies resolver to User

---
+++ src/useCases/User/container/index.ts
@@ -0,0 +1,5 @@
+import { container } from 'tsyringe';
+import FakeUsersRepository from '@entities/User/FakeUserRepository';
+import IUserRepository from '@entities/User/IUserRepository';
+
+container.registerSingleton<IUserRepository>('UsersRepository', FakeUsersRepository);
