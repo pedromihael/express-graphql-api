e0f1d48ccd59fc512a0d9234a4c4778d75f26e0b
Pedro Mihael <pedromvpaes@gmail.com>
2021-01-22 10:51:05 -0400

    test: ensure list resolvers will list no users with arg regex not matched

--- src/useCases/User/resolvers/ListUsers/ListUsersResolver.spec.ts
+++ src/useCases/User/resolvers/ListUsers/ListUsersResolver.spec.ts
@@ -50,5 +50,25 @@ describe('ListUsersResolver', () => {
     expect(result).not.toEqual(expect.arrayContaining([userData4]));
   });
 
-  // it('should list no user if arg providade does not match regex');
+  it('should list no user if arg providade does not match regex', async () => {
+    const userId2 = uuidv4();
+    const userId3 = uuidv4();
+    const userId4 = uuidv4();
+
+    const userData1 = new UserBuilder(userId).setName('Pedro').build();
+    const userData2 = new UserBuilder(userId2).setName('Pencio').build();
+    const userData3 = new UserBuilder(userId3).setName('abcpedro').build();
+    const userData4 = new UserBuilder(userId4).setName('mihael').build();
+
+    const userDatas = [userData1, userData2, userData3, userData4];
+
+    userDatas.map(async (userData) => {
+      await usersRepository.create(userData);
+    });
+
+    const resolver = await listUsersResolver.execute();
+    const result = await resolver.Query.list('', { name: 'Camila' }, {});
+
+    expect(result).not.toEqual(expect.arrayContaining(userDatas));
+  });
 });
