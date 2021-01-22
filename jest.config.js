bc05d9dc52409b5a8392dde1116d02777a391666
Pedro Mihael <pedromvpaes@gmail.com>
2021-01-21 22:37:33 -0400

    chore: update config files

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
-import { compilerOptions } from './tsconfig.json';
-import { pathsToModuleNameMapper } from 'ts-jest/utils';
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
 
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/en/configuration.html
- */
-
-export const clearMocks = true;
-export const coverageProvider = 'v8';
-export const projects = 'ts-jest';
-export const testEnvironment = 'node';
-export const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' });
+module.exports = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  projects: ['ts-jest'],
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
+  testMatch: ['**/*.spec.ts'],
+};
